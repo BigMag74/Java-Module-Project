deadf1fe6d369751909c35d87d9b38521598b3f7
BigMag74 <bigmag174@yandex.ru>
2022-10-25 15:33:28 +0500

    Проектная работа №1

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,34 @@
+import static java.lang.Math.floor;
+
+public class Calculator {
+    int peronCount;
+    double sum = 0;
+    String items = "";
+
+    Calculator(int peronCount){
+        this.peronCount = peronCount;
+    }
+
+    void addItem(Item item) {
+        sum += item.price;
+        items += item.name + "\n";
+    }
+    void showResult(){
+        System.out.println("Добавленные товары:");
+        System.out.println(items);
+        double priceForPerson = sum/peronCount;
+        String endOfString = " рублей";
+        switch ((int)floor(priceForPerson)%10){
+            case 1:
+                endOfString = " рубль";
+                break;
+            case 2:
+            case 3:
+            case 4:
+                endOfString = " рубля";
+                break;
+        }
+        System.out.println("Каждый человек должен заплатить " + String.format("%.2f", priceForPerson) + endOfString);
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,56 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // ваш код начнется здесь
         // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        System.out.println("Здраствуйте, на скольких человек необходимо разделить счет?");
+        int peronCount;
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            try {
+                peronCount = Integer.parseInt(scanner.next());
+            }
+            catch (NumberFormatException ex) {
+                System.out.println("Ошибка \nПожалуйста введите корректное значение");
+                continue;
+            }
+            if (peronCount == 1)
+                System.out.println("Вы здесь один, счёт делить не с кем \nПожалуйста введите корректное значение");
+            else if (peronCount > 1)
+                break;
+            else
+                System.out.println("Ошибка \nПожалуйста введите корректное значение");
+        }
+        Calculator calculator = new Calculator(peronCount);
+        String answer;
+        while (true) {
+            Item item = new Item();
+
+            System.out.println("Ведите название товара");
+            item.name = scanner.next();
+            System.out.println("Ведите стоимость товара  формате \"рубли.копейки\"");
+            try{
+                item.price = Double.parseDouble(scanner.next());
+            }
+            catch (NumberFormatException ex){
+                System.out.println("Ошибка при добавлении товара, попробуйте снова");
+                continue;
+            }
+            if(item.price < 0) {
+                System.out.println("Ошибка при добавлении товара, попробуйте снова");
+                continue;
+            }
+
+            calculator.addItem(item);
+            System.out.println("Товар успешно добавлен");
+            System.out.println("Хотите ли добавить еще один товар?");
+            answer = scanner.next();
+            if(answer.equalsIgnoreCase("Завершить"))
+                break;
+        }
+        calculator.showResult();
     }
 }
